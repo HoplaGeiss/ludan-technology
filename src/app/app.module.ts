a8e1c544b33c5dc8b3a2bc031a6fe5616e7dd700
Gabriel <gabriel.muller.12@gmail.com>
2018-12-05 09:49:33 +0000

    initial deploy on firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCBzAMDkJusreJD_GzVpKFT2QBtex9xExk",
+    authDomain: "ludan-technology.firebaseapp.com",
+    databaseURL: "https://ludan-technology.firebaseio.com",
+    projectId: "ludan-technology",
+    storageBucket: "ludan-technology.appspot.com",
+    messagingSenderId: "85689017373"
+  }
 };
 
 /*
