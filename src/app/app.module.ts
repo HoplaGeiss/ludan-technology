46ed54278285d15116bdc7cf16046b96f33f7c36
Gabriel Muller <hopla@ifilax.home>
2018-12-14 16:55:11 +0000

    Added animation on the thumbnail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { PortfolioService } from './shared/services/portfolio.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { NavbarModule } from './shared/components/navbar/navbar.module';
+import { PortfolioService } from './shared/services/portfolio.service';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NavbarModule,
     AngularFireModule.initializeApp(environment.firebase),
--- src/app/shared/components/catalogue/thumbnail/thumbnail.component.ts
+++ src/app/shared/components/catalogue/thumbnail/thumbnail.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter, HostListener, ElementRef } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'ludan-thumbnail',
   styleUrls: ['thumbnail.component.scss'],
   template: `
+    <!-- prettier-ignore -->
     <div
       class="thumbnail"
       [style.backgroundImage]="'url(assets/images/' + img + '.png)'"
       (mouseover)="hover = true"
       (mouseleave)="hover = false"
-    >
+      [@scrollAnimation]="show ? 'show' : 'hide'
+      ">
       <div *ngIf="hover" class="hover-container" (click)="clickEvent.emit($event)">
         <div class="hover-text-wrapper">
           <div class="title">{{ name }}</div>
@@ -17,13 +19,49 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         </div>
       </div>
     </div>
-  `
+  `,
+  animations: [
+    trigger('scrollAnimation', [
+      state(
+        'show',
+        style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        })
+      ),
+      state(
+        'hide',
+        style({
+          opacity: 0,
+          transform: 'translateY(60px)'
+        })
+      ),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('700ms ease-in'))
+    ])
+  ]
 })
 export class ThumbnailComponent {
   hover: boolean;
+  show = true;
 
   @Input() img: string;
   @Input() name: string;
 
   @Output() clickEvent = new EventEmitter();
+
+  constructor(public elementRef: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.elementRef.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+
+    if (scrollPosition + windowHeight - 200 >= componentPosition) {
+      this.show = true;
+    } else {
+      this.show = false;
+    }
+  }
 }
